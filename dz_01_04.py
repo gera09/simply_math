c128878834720af22b96f704fa7cf697290d9de4
gera09 <bp-sva@yandex.ru>
2019-10-15 19:44:09 +0300

    4

--- dz_01_04.py
+++ dz_01_04.py
@@ -1,9 +1,8 @@
-import matplotlib as matplotlib
-# %matplotlib inline
 import numpy as np
 import matplotlib.pyplot as plt
-
-x = np.linspace(0, 10, 21)
-plt.plot(x, x, marker="o")
-print(x)
+x = np.linspace(-10, 10, 121)
+plt.plot(x, np.cos(x))
+z = x * 0.95
+plt.plot(x, np.cos(z))
+print(x, z)
 
