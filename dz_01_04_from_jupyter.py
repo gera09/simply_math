2d557e4162226dba841f0acd3cddff6c14d9b9e2
gera09 <bp-sva@yandex.ru>
2020-03-25 20:06:38 +0300

    123

--- dz_01_04_from_jupyter.py
+++ dz_01_04_from_jupyter.py
@@ -7,15 +7,11 @@ import IPython
 get_ipython().run_line_magic('matplotlib', 'inline')
 import numpy as np
 import matplotlib.pyplot as plt
+
 x = np.linspace(-10, 10, 121)
 plt.plot(x, np.cos(x))
 z = x * 0.95
 plt.plot(x, np.cos(z))
 print(x, z)
 
-
 # In[ ]:
-
-
-
-
